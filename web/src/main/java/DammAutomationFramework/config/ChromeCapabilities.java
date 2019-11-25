0057787bee9da876d0a0c4e13940e6093b9af408
Alexander Skalatskiy <alexander.skalatskiy@mindata.es>
2019-11-25 14:24:36 +0100

    Added ChromeOptions clas to config folder

---
+++ web/src/main/java/DammAutomationFramework/config/ChromeCapabilities.java
@@ -0,0 +1,17 @@
+package DammAutomationFramework.config;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class ChromeCapabilities extends DesiredCapabilities {
+
+    public ChromeCapabilities() {
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+        options.addArguments("--start-maximized");
+        options.addArguments("incognito");
+        options.addArguments("disable-infobars");
+        // options.addArguments("no-sandbox");
+        setCapability(ChromeOptions.CAPABILITY, options);
+    }
+}
