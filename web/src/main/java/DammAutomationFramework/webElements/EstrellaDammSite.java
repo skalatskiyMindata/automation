0cb58b698acfc441dcec1a5d6ff8aa5a2c063a02
Alexander Skalatskiy <alexander.skalatskiy@mindata.es>
2019-12-02 16:09:08 +0100

    topbar implementation

--- web/src/main/java/DammAutomationFramework/webElements/EstrellaDammSite.java
+++ web/src/main/java/DammAutomationFramework/webElements/EstrellaDammSite.java
@@ -3,6 +3,7 @@ package DammAutomationFramework.webElements;
 import DammAutomationFramework.webElements.component.CookiesComponent;
 import DammAutomationFramework.webElements.component.AccessGenericComponent;
 import DammAutomationFramework.webElements.component.RegisterComponent;
+import DammAutomationFramework.webElements.component.TopBarComponent;
 import DammAutomationFramework.webElements.page.estrellaDamm.*;
 import com.github.webdriverextensions.WebSite;
 import org.slf4j.Logger;
@@ -22,6 +23,7 @@ public class EstrellaDammSite extends WebSite {
     public CookiesComponent cookiesComponent;
     public RegisterComponent registerComponent;
     public AccessGenericComponent accessGenericComponent;
+    public TopBarComponent topBarComponent;
 
     // Pages
     public HomePage mainPage;
